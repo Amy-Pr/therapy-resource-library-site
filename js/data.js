53069f513224d148466bba3f0d47be2a1c884635
amy_pr <ampreci@gmail.com>
2023-02-09 17:41:59 -0800

    initial commit

---
+++ js/data.js
@@ -0,0 +1,23 @@
+const dataResources = [
+    {title: "The Literacy Shed",
+     link: "https://www.literacyshed.com/",
+     description: "A collection of short videos, mostly animated, some wordless.",
+     tags: ["all", "videos", "language"]
+    },
+
+    {title: "Boom Cards",
+     link: "https://wow.boomlearning.com/",
+     description: "Great activities for on-screen use.",
+     tags: ["all", "language", "articulation", "fluency", "pragmatics",  "games"]
+    },
+
+    {title: "ReadWorks",
+     link: "https://www.readworks.org/",
+     description: "Tons of free reading material searchable by grade, goals, and genre.",
+     tags: ["all", "stories", "expository text"]
+    }
+    
+  ]
+
+
+  
